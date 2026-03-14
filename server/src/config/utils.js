c31a5d7934222a8645c20209a003d8586f2e0ef4
tomarmayankk <mayanktomarpc@gmail.com>
2026-03-14 21:14:40 +0530

    updated JWT token file

--- server/src/config/utils.js
+++ server/src/config/utils.js
@@ -1,15 +1,16 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "7d"
-    });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: "strict",
-        secure: false,
-    });
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "None",   // allow cross-site cookies
+    secure: true        // required for HTTPS
+  });
 
-    return token;
+  return token;
 };
