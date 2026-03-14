6d5d5d425036d8ffa730083b7ca1138fa72b88ed
tomarmayankk <mayanktomarpc@gmail.com>
2026-03-14 20:32:14 +0530

    axios ready

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://finance-manager-backend-dcve.onrender.com/api",
   withCredentials: true, // VERY IMPORTANT for cookies
 });
 
